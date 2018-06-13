faf24f2b7ced34d2fec168960de7d6b045f740e1
Sharpy <cam-chester@hotmail.co.uk>
2018-06-13 02:35:09 +0100

    Added Probuilder and a few other things
    Added probuilder
    Added new map
    Added script that makes the enemy health bar always face the player

---
+++ Assets/Scripts/FacePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour {
+
+    public GameObject PlayerObj;
+    private Transform PlayerTransform;
+
+	void Start () {
+        PlayerObj = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void Update () {
+        PlayerTransform = PlayerObj.transform;
+        this.transform.LookAt(PlayerTransform);
+	}
+}
